ab39a9f8aff31215b68a3a632ae3efffa8f44dc5
Jose Epifanio <jose.epifanio90@gmail.com>
2017-04-02 21:46:27 -0500

    forgot password email sender.

--- app/mailers/postman_mailer.rb
+++ app/mailers/postman_mailer.rb
@@ -1,2 +1,10 @@
 class PostmanMailer < ApplicationMailer
+  default from: 'atomtechnologiesinc@gmail.com'
+
+  def send_password(user, user_info)
+    @user = user.user
+    @email = user_info.email
+    @pwd = user.password_digest
+    mail(to: @email, subject: 'Forgot your password?')
+  end
 end
